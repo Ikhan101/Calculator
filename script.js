67145a41deb568be29340596ffdcd1d22d8330a3
Ikhan101 <c1008166@my.shu.ac.uk>
2023-03-02 19:11:29 +0000

    js

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let buffer = "0";
 let previousOperator;
 
 const screen = document.querySelector('.screen');
+
 function buttonClick(value){
     if(isNaN(value)){
         handleSymbol(value);
     }else{
         handleNumber(value);
     }
-    screen.innerText-buffer;
+    screen.innerText = buffer;
 }
 
 function handleSymbol(symbol){
