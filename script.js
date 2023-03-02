7d1fb0823de341fbcba7e6c76208b8753c4933b6
Ikhan101 <c1008166@my.shu.ac.uk>
2023-03-02 14:18:38 +0000

    js

--- script.js
+++ script.js
@@ -79,7 +79,6 @@ function handleNumber(numberString){
         buffer += numberString;
     }
 }
-
 function init(){
     document.querySelector('.calc-buttons').addEventListener('click', function(event){
         buttonClick(event.target.innerText);
